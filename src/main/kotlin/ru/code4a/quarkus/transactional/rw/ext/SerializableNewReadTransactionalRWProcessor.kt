02fa3bed98800039d9c30405fb3512aa537a9fa2
Evgeniy Simonenko <tiikara93@gmail.com>
2024-07-29 14:48:57 +1000

    Add Transactional Processors Producers
    Introduce TransactionalRWExtProcessorsProducer to manage different NewReadTransactionalRWProcessor instances based on build properties. Removed IfBuildProperty annotations from SerializableNewReadTransactionalRWProcessor and HibernateNewReadTransactionalRWProcessor classes, delegating the conditional creation logic to the producer class.

--- src/main/kotlin/ru/code4a/quarkus/transactional/rw/ext/SerializableNewReadTransactionalRWProcessor.kt
+++ src/main/kotlin/ru/code4a/quarkus/transactional/rw/ext/SerializableNewReadTransactionalRWProcessor.kt
@@ -1,12 +1,10 @@
 package ru.code4a.quarkus.transactional.rw.ext
 
-import io.quarkus.arc.properties.IfBuildProperty
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.persistence.EntityManager
 import ru.code4a.quarkus.transactional.rw.processor.NewReadTransactionalRWProcessor
 
 @ApplicationScoped
-@IfBuildProperty(name = "ru.code4a.transactional-rw.serializable.enabled", stringValue = "true")
 class SerializableNewReadTransactionalRWProcessor(
   private val entityManager: EntityManager
 ) : NewReadTransactionalRWProcessor {
